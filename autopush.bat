75d518703abd465f67212b6703622e28c97cfe20
he he <wkf16@example.com>
2023-11-19 13:31:45 +0800

    update 2023-11-19T13:31:45

--- autopush.bat
+++ autopush.bat
@@ -1,11 +1,12 @@
 @echo off
 
-:: 设置日期和时间格式
-set "BackupTime=%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
+:: 获取当前的ISO 8601格式日期和时间（格式：YYYY-MM-DDTHH:MM:SS）
+for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set datetime=%%a
+set "datetimestamp=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%T%datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%"
 
 :: 进行git操作
 git add .
-git commit -m "update %BackupTime%"
+git commit -m "update %datetimestamp%"
 git push --force
 
 echo Git操作完成: 已经添加、提交并推送了代码。
