70d6c37ce00c4b49b24d97bc68836c7e2bdccaf1
tr <Administrator@USER-20230509MF>
2023-12-06 16:19:45 +0800

    修改了模型参数

--- Model_create.m
+++ Model_create.m
@@ -2,24 +2,24 @@ clear all
 clc
 
 % 定义机器人模型
-ML2 = Link([pi/2  327     0     pi/2      0   ]);
-ML3 = Link([pi/2   0     225     0        0   ]);
-ML4 = Link([0      0      0     pi/2      0   ]);
-ML5 = Link([0     100     0    -pi/2      0   ]);
-ML6 = Link([0      0      0     pi/2      0   ]);
-ML7 = Link([0     050      0       0       0   ]);
+ML2 = Link([0  0.399      0      pi/2       0   ]);
+ML3 = Link([0   0       0.35     0          0   ]);
+ML4 = Link([0   0      0.042     pi/2       0   ]);
+ML5 = Link([0   0.451     0      - pi/2     0   ]);
+ML6 = Link([0   0         0      pi/2       0   ]);
+ML7 = Link([0   0.082     0       0         0   ]);
 
 % 定义关节角度限制（弧度）
-joint1_angle_max = 230/180*pi;
-joint1_angle_min = -230/180*pi;
-joint2_angle_max = 113/180*pi + pi/2;
-joint2_angle_min = -115/180*pi + pi/2;
-joint3_angle_max = 55/180*pi;
-joint3_angle_min = -205/180*pi;
-joint4_angle_max = 230/180*pi;
-joint4_angle_min = -230/180*pi;
-joint5_angle_max = 120/180*pi;
-joint5_angle_min = -125/180*pi;
+joint1_angle_max = 170/180*pi;
+joint1_angle_min = -170/180*pi;
+joint2_angle_max = 130/180*pi + pi/2;
+joint2_angle_min = -100/180*pi + pi/2;
+joint3_angle_max = 70/180*pi;
+joint3_angle_min = -200/180*pi;
+joint4_angle_max = 270/180*pi;
+joint4_angle_min = -270/180*pi;
+joint5_angle_max = 130/180*pi;
+joint5_angle_min = -130/180*pi;
 joint6_angle_max = 400/180*pi;
 joint6_angle_min = -400/180*pi;
 
